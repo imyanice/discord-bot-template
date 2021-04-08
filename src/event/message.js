75f511df33c1a54caa52641aceb1b4ed9af622d2
Yan-Jobs <yan@yanjobs.me>
2021-04-08 15:32:11 +0200

    Fix

--- src/event/message.js
+++ src/event/message.js
@@ -1,4 +1,10 @@
 const BaseEvent = require('../utils/BaseEvent.js");
                           
 module.exports = class MessageEvent extends BaseEvent {
+  constructor() {
+    super("message")
+  }
+  async run() {
+    //I come later to finish the work...
+  }
 }
